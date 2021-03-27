04ec842d122cd04fba694a6ec4ad0cb728505b4a
spacholski1225 <spacholski1225@gmail.com>
2021-03-27 13:58:12 +0100

    extend RecpiesController about new methos whos get recipes by ingredients

--- FridgeBook/FridgeBook/Controllers/RecipesController.cs
+++ FridgeBook/FridgeBook/Controllers/RecipesController.cs
@@ -19,7 +19,7 @@ namespace FridgeBook.Controllers
         }
 
         [HttpPost]
-        public void AddRecipeIntoDatabase() //jaki tu powinine byc zwracany typ
+        public ActionResult<Pizza> AddRecipeIntoDatabase() //jaki tu powinine byc zwracany typ
         {
             Pizza record = new Pizza
             {
@@ -27,13 +27,32 @@ namespace FridgeBook.Controllers
             };
 
              _context.InsertRecord<Pizza>("Pizza", record);
+            return Ok();
         }
         
         [HttpGet]
-        public void GetRecipeByIngredients(List<string> ingredients) //trzeba dorobic metode w MongoDbCRUD ktora znajduje rekordy z zawieranymi skladnikami
+        public ActionResult<IEnumerable<Pizza>> GetRecipeByIngredients(List<string> ingredients) //trzeba dorobic metode w MongoDbCRUD ktora znajduje rekordy z zawieranymi skladnikami
         {
-
             var listOfPizzas = _context.LoadRecords<Pizza>("Pizza");
+            var pizzasWithIngredients = new List<Pizza>();
+            var newPizza = new Pizza();
+            foreach (var pizza in listOfPizzas)
+            {
+                foreach (var pizzaIngredient in pizza.MustHaveIngredients)
+                {
+                    if (ingredients.Contains(pizzaIngredient))
+                    {
+                        newPizza.MustHaveIngredients.Add(pizzaIngredient);
+                    }
+                }
+                if(pizza == newPizza) // porownanie dwoch obiektow czy ich wartosci tj lista MustHaveIngredients jest taka sama
+                                        //jesli tak to dodac obiekt pizza do listy pizz zawierajacych te skladniki
+                {
+                    pizzasWithIngredients.Add(pizza);
+                }
+            }
+
+            return Ok(pizzasWithIngredients);
 
             //przeszukiwanie listy pizza przez liste ingredients tak aby pasujace do siebie elementy byly
             //sprawdzane i zostala zwrocona lista obiektow pizza ktora zawiera pasujace skladniki
